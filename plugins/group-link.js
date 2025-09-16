21cc6c648d127fa9e17048b8fc542c7185ecc0b0
AngelBotsD <angelperez22448@gmail.com>
2025-09-16 03:20:02 -0600

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,1 +1,25 @@
-// Archivo de prueba para plugins
+import fetch from 'node-fetch'
+
+var handler = async (m, { conn }) => {
+    try {
+        let link = '🗡️ https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat)
+        let ppUrl = await conn.profilePictureUrl(m.chat, 'image').catch(() => null)
+
+        if (ppUrl) {
+            await conn.sendMessage(m.chat, { image: { url: ppUrl }, caption: link }, { quoted: m })
+        } else {
+            await conn.sendMessage(m.chat, { text: link }, { quoted: m })
+        }
+
+    } catch (error) {
+        console.error(error)
+        conn.reply(m.chat, '⚠ No se pudo obtener el enlace del grupo. Asegúrate de que soy administrador.', m)
+    }
+}
+
+handler.customPrefix = /^(\.link|link)$/i
+handler.command = new RegExp
+handler.group = true
+handler.admin = true
+
+export default handler
