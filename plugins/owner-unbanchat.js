4ba4debfca8044bc09559207ba2335619adc23a1
AngelBotsD <angelperez22448@gmail.com>
2025-09-16 22:58:21 -0600

    Crear owner-unbanchat.js

---
+++ plugins/owner-unbanchat.js
@@ -0,0 +1,12 @@
+let handler = async (m, { conn, isAdmin, isROwner} ) => {
+    if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
+    global.db.data.chats[m.chat].isBanned = false
+    m.reply('> ya volví jefecito')   
+}
+handler.help = ['desbanearbot']
+handler.tags = ['group']
+handler.customPrefix = /^(\.unbanchat|vuelve bot pendejo)$/i
+handler.command = new RegExp
+handler.group = true 
+handler.owner = true
+export default handler
